0ee0e20e519bbd9901e6609c4e53d121e0af4bda
yokomixi1031 <domybestflyhigh@gmail.com>
2022-02-24 13:00:38 +0900

    Issue1~3の修正 2回目

--- app/controllers/calendars_controller.rb
+++ app/controllers/calendars_controller.rb
@@ -30,13 +30,12 @@ class CalendarsController < ApplicationController
     plans = Plan.where(date: @todays_date..@todays_date + 6)
 
     7.times do |x|
-      today_plans = []
+      @today_plans = []
       plans.each do |plan|
-        today_plans.push(plan.plan) if plan.date == @todays_date + x
+        @today_plans.push(plan.plan) if plan.date == @todays_date + x
       end
-      days = { :month: (@todays_date + x).month, :date: (@todays_date+x).day, :plans: today_plans}
+      @week_days = { :month: (@todays_date + x).month, :date: (@todays_date + x).day, :plans: @today_plans}
       @week_days.push(days)
     end
-
   end
 end
